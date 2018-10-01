3da6fba259c22bc1b4bce4e592b8300f3fb3a9a3
dobrichev@gmail.com <dobrichev@gmail.com>
2018-10-01 23:59:50 +0300

    char *const --> const char*

--- go_4xx_cpp.h
+++ go_4xx_cpp.h
@@ -25,7 +25,7 @@ void Go_c400_40p(char *ze, int task) {// work on bitfields std puz
 	memset(d_units, 0, sizeof d_units);
 	memset(c_bands, 0, sizeof c_bands);
 	memset(c_units, 0, sizeof c_units);
-	char *const rcb  = "RCB";
+	const char* rcb  = "RCB";
 	for (int i = 0; i < 81; i++)if (ze[i] != '.') {
 		pattern.Set_c(i);
 		int d = ze[i] - '1',bit=1<<d;
@@ -267,7 +267,7 @@ void Go_cxx() {
 
 void Go_c440(){
 	cout << "Go_440 entry " << sgo.finput_name  <<" game results to parse"<< endl;
-	int v = sgo.vx[0];
+	//int v = sgo.vx[0];
 	char * ze = finput.ze;
 	finput.open(sgo.finput_name);
 	if (!finput.is_open()){
@@ -338,7 +338,7 @@ void Go_c480() {//add  compressed clues to entry
 		cerr << "missing base out" << endl;
 		return;
 	}
-	int update = sgo.vx[0];
+	//int update = sgo.vx[0];
 	finput.open(sgo.finput_name);
 	if (!finput.is_open()) {
 		cerr << "error open file " << sgo.finput_name << endl;
@@ -347,7 +347,7 @@ void Go_c480() {//add  compressed clues to entry
 	char ze[200]; ze[81] = ';';
 	int lcmp = 0;
 	while (finput.GetPuzzle(ze)) {
-		int n = 0, p = 82;
+		int /*n = 0,*/ p = 82;
 		for (int i = 9; i < 81; i++)if (ze[i] - '.')
 			ze[p++] = ze[i];
 
@@ -404,7 +404,7 @@ void Go_c481() {//base check -i ads -s1- base -o add root
 			}
 		}
 		else lcmp = ll;
-		int lbase = (int)strlen(zcompress);
+		//int lbase = (int)strlen(zcompress);
 		// read/ out if update base as long as below
 	loop_base:
 		int icomp = strcmp(zcompress, zeb);
